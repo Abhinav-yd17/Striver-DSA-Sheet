1d458a3e7dd0006da8b0f02bb0657fffef8ac13f
Abhinav Yadav <abhinavyadav.ay17@gmail.com>
2026-08-31 22:22:28 +0530

    changes

---
+++ DP/(1)70. Climbing Stairs.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int climbStairs(int n) {
+        long long a = 1, b = 1;  
+        for(int i = 0; i < n; i++){
+            long long temp = a;
+            a = a + b;
+            b = temp;
+        }
+        return b;
+    }
+};
